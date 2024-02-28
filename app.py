43e0848817fb2776979b0bf6418201d62741bf42
DakshRathi <117447434+DakshRathi@users.noreply.github.com>
2024-02-28 22:20:57 +0530

    added registration gui

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ class NLPApp:
         self.root.mainloop()
 
     def login_gui(self):
+        self.clear()
 
         heading = Label(self.root,text='NLPApp', bg='#2b4370',fg='white')
         heading.pack(pady=(30,30))
@@ -35,7 +36,41 @@ class NLPApp:
         label3 = Label(self.root, text='Not a member?', bg='#2b4370',fg='white')
         label3.pack(pady=(20, 10))
 
-        redirect_btn = Button(self.root, text='Register Now')
+        redirect_btn = Button(self.root, text='Register Now', bg='#2b4370', command=self.register_gui)
+        redirect_btn.pack(pady=(10, 10))
+
+    def register_gui(self):
+        self.clear()
+
+        heading = Label(self.root, text='NLPApp', bg='#2b4370',fg='white')
+        heading.pack(pady=(30, 30))
+        heading.configure(font=('verdana', 24, 'bold'))
+
+        label0 = Label(self.root, text='Enter Name', bg='#2b4370',fg='white')
+        label0.pack(pady=(10, 10))
+
+        self.name_input = Entry(self.root, width=35)
+        self.name_input.pack(pady=(5, 10), ipady=4)
+
+        label1 = Label(self.root, text='Enter Email', bg='#2b4370',fg='white')
+        label1.pack(pady=(10, 10))
+
+        self.email_input = Entry(self.root, width=35)
+        self.email_input.pack(pady=(5, 10), ipady=4)
+
+        label2 = Label(self.root, text='Enter Password', bg='#2b4370',fg='white')
+        label2.pack(pady=(10, 10))
+
+        self.password_input = Entry(self.root, width=35, show='*')
+        self.password_input.pack(pady=(5, 10), ipady=4)
+
+        register_btn = Button(self.root, text='Register', width=30, height=2)
+        register_btn.pack(pady=(10, 10))
+
+        label3 = Label(self.root, text='Already a member?', bg='#2b4370',fg='white')
+        label3.pack(pady=(20, 10))
+
+        redirect_btn = Button(self.root, text='Login Now', command=self.login_gui)
         redirect_btn.pack(pady=(10, 10))
 
     def clear(self):
